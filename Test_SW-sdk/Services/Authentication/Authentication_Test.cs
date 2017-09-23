b1b6cd8725a31fcf6538237c13bf785ca1b2d640
Hermes Jimenez <hermes.jimenez@sw.com.mx>
2017-09-23 16:51:19 -0500

    SDT-802 Se corrigen problemas con concurrencia para las pruebas unitarias.

--- Test_SW-sdk/Services/Authentication/Authentication_Test.cs
+++ Test_SW-sdk/Services/Authentication/Authentication_Test.cs
@@ -8,60 +8,38 @@ namespace Test_SW.Services.Authentication_Test
     [TestClass]
     public class Authentication_Test
     {
-        private BuildSettings Build;
-        private object resultExpect;
-        private void GetEnviromentVariables()
-        {
-            if (Environment.GetEnvironmentVariable("sw-sdk-url") != null)
-            {
-                Build.Url = Environment.GetEnvironmentVariable("sw-sdk-url");
-            }
-            if (Environment.GetEnvironmentVariable("sw-sdk-user") != null)
-            {
-                Build.User = Environment.GetEnvironmentVariable("sw-sdk-user");
-            }
-            if (Environment.GetEnvironmentVariable("sw-sdk-password") != null)
-            {
-                Build.Password = Environment.GetEnvironmentVariable("sw-sdk-password");
-            }
-            if (Environment.GetEnvironmentVariable("sw-sdk-token") != null)
-            {
-                Build.Token = Environment.GetEnvironmentVariable("sw-sdk-token");
-            }
-        }
         [TestMethod]
         public void ValidateAuthentication()
         {
-            Build = new BuildSettings();
-            GetEnviromentVariables();
-            Authentication auth = new Authentication(Build.Url, Build.User, Build.Password);
+            var build = new BuildSettings();
+            Authentication auth = new Authentication(build.Url, build.User, build.Password);
             var response = auth.GetToken();
             Assert.IsTrue(!string.IsNullOrEmpty(response.Data.token));
         }
         [TestMethod]
         public void ValidateExistUser()
         {
-            Build = new BuildSettings();
-            resultExpect = "Falta Capturar Usuario";
-            Authentication auth = new Authentication(Build.Url, "", Build.Password);
+            var build = new BuildSettings();
+            var resultExpect = "Falta Capturar Usuario";
+            Authentication auth = new Authentication(build.Url, "", build.Password);
             var response = auth.GetToken();
             Assert.AreEqual(response.message, (string)resultExpect, (string)resultExpect);
         }
         [TestMethod]
         public void ValidateExistPassword()
         {
-            Build = new BuildSettings();
-            resultExpect = "Falta Capturar Contraseña";
-            Authentication auth = new Authentication(Build.Url, Build.User, "");
+            var build = new BuildSettings();
+            var resultExpect = "Falta Capturar Contraseña";
+            Authentication auth = new Authentication(build.Url, build.User, "");
             var response = auth.GetToken();
             Assert.AreEqual(response.message, (string)resultExpect, (string)resultExpect);
         }
         [TestMethod]
         public void ValidateExistUrl()
         {
-            Build = new BuildSettings();
-            resultExpect = "Falta Capturar URL";
-            Authentication auth = new Authentication("", Build.User, Build.Password);
+            var build = new BuildSettings();
+            var resultExpect = "Falta Capturar URL";
+            Authentication auth = new Authentication("", build.User, build.Password);
             var response = auth.GetToken();
             Assert.AreEqual(response.message, (string)resultExpect, (string)resultExpect);
         }
--- Test_SW-sdk/Services/Cancelation/Cancelation_Test.cs
+++ Test_SW-sdk/Services/Cancelation/Cancelation_Test.cs
@@ -10,73 +10,37 @@ namespace Test_SW.Services.Cancelation_Test
     [TestClass]
     public class Cancelation_Test
     {
-        private BuildSettings Build = new BuildSettings();
-        private object resultExpect;
-        private void GetEnviromentVariables()
-        {
-            if (Environment.GetEnvironmentVariable("sw-sdk-url") != null)
-            {
-                Build.Url = Environment.GetEnvironmentVariable("sw-sdk-url");
-            }
-            if (Environment.GetEnvironmentVariable("sw-sdk-user") != null)
-            {
-                Build.User = Environment.GetEnvironmentVariable("sw-sdk-user");
-            }
-            if (Environment.GetEnvironmentVariable("sw-sdk-password") != null)
-            {
-                Build.Password = Environment.GetEnvironmentVariable("sw-sdk-password");
-            }
-            if (Environment.GetEnvironmentVariable("sw-sdk-token") != null)
-            {
-                Build.Token = Environment.GetEnvironmentVariable("sw-sdk-token");
-            }
-            if (Environment.GetEnvironmentVariable("sw-sdk-cer") != null)
-            {
-                Build.Cer = Environment.GetEnvironmentVariable("sw-sdk-cer");
-            }
-            if (Environment.GetEnvironmentVariable("sw-sdk-key") != null)
-            {
-                Build.Key = Environment.GetEnvironmentVariable("sw-sdk-key");
-            }
-            if (Environment.GetEnvironmentVariable("sw-sdk-cer-password") != null)
-            {
-                Build.CerPassword = Environment.GetEnvironmentVariable("sw-sdk-cer-password");
-            }
-        }
         [TestMethod]
         public void CancelationByCSD()
         {
-            Build = new BuildSettings();
-            GetEnviromentVariables();
-            Cancelation cancelation = new Cancelation(Build.Url, Build.User, Build.Password);
-            var response = cancelation.CancelarByCSD(Build.Cer, Build.Key, Build.Rfc, Build.CerPassword, "01724196-ac5a-4735-b621-e3b42bcbb459");
+            var build = new BuildSettings();
+            Cancelation cancelation = new Cancelation(build.Url, build.User, build.Password);
+            var response = cancelation.CancelarByCSD(build.Cer, build.Key, build.Rfc, build.CerPassword, "01724196-ac5a-4735-b621-e3b42bcbb459");
             Assert.IsTrue(response.Data.Acuse != null && response.status == "success");
         }
         [TestMethod]
         public void CancelationByPFX()
         {
-            Build = new BuildSettings();
-            GetEnviromentVariables();
-            Cancelation cancelation = new Cancelation(Build.Url, Build.User, Build.Password);
-            var response = cancelation.CancelarByPFX(Build.Pfx, Build.Rfc, Build.CerPassword, "01724196-ac5a-4735-b621-e3b42bcbb459");
+            var build = new BuildSettings();
+            Cancelation cancelation = new Cancelation(build.Url, build.User, build.Password);
+            var response = cancelation.CancelarByPFX(build.Pfx, build.Rfc, build.CerPassword, "01724196-ac5a-4735-b621-e3b42bcbb459");
             Assert.IsTrue(response.Data.Acuse != null && response.status == "success");
         }
         [TestMethod]
         public void CancelationByXML()
         {
-            Build = new BuildSettings();
-            GetEnviromentVariables();
-            Cancelation cancelation = new Cancelation(Build.Url, Build.User, Build.Password);
-            var response = cancelation.CancelarByXML(Build.Acuse);
+            var build = new BuildSettings();
+            Cancelation cancelation = new Cancelation(build.Url, build.User, build.Password);
+            var response = cancelation.CancelarByXML(build.Acuse);
             Assert.IsTrue(response.Data.Acuse != null && response.status == "success");
         }
         [TestMethod]
         public void ValidateParameters()
         {
-            resultExpect = "Son necesarios el .Cer y el .Key en formato B64";
-            Build = new BuildSettings();
-            Cancelation cancelation = new Cancelation(Build.Url, Build.User, Build.Password);
-            var response = cancelation.CancelarByCSD(Build.Cer, Build.Key, Build.Rfc, Build.CerPassword, "");
+            var resultExpect = "Son necesarios el .Cer y el .Key en formato B64";
+            var build = new BuildSettings();
+            Cancelation cancelation = new Cancelation(build.Url, build.User, build.Password);
+            var response = cancelation.CancelarByCSD(build.Cer, build.Key, build.Rfc, build.CerPassword, "");
             Assert.IsTrue(response.messageDetail.Contains((string)resultExpect));
         }
     }
