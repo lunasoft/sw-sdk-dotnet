68fc67d5c179b92e016a2c58b7dc6894cce9e287
alxcode <ing.gramos@outlook.com>
2018-08-20 12:30:10 -0500

    SDT-1047 Added Acept/Reject request method

---
+++ SW-sdk/Services/AcceptReject/AcceptRejectResponse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using SW.Helpers;
+using SW.Entities;
+using System.Runtime.Serialization;
+using System;
+
+namespace SW.Services.AcceptReject
+{
+    public class AcceptRejectResponse : Response
+    {
+        [DataMember(Name = "data")]
+        public Data data { get; set; }
+    }
+    public partial class Data
+    {
+        [DataMember(Name = "acuse")]
+        public string acuse { get; set; }
+        [DataMember]
+        public Dictionary<string, string> uuid { get; set; }
+    }
+    public class AcceptRejectAuthorizeResponse : AcceptRejectResponse
+    {
+        public string codStatus { get; set; }
+        public List<invoicesStatus> folios { get; set; }
+    }
+
+    public class invoicesStatus
+    {
+        public Guid uuid { get; set; }
+        public string estatusUUID { get; set; }
+        public string respuesta { get; set; }
+    }
+}
---
+++ Test_SW-sdk/Services/AcceptReject/AcceptReject_Test.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using SW.Helpers;
+using Test_SW.Helpers;
+using SW.Services.AcceptReject;
+
+namespace Test_SW.Services.AcceptReject_Test
+{
+    [TestClass]
+    public class AcceptReject_Test
+    {
+        [TestMethod]
+        public void ValidateParameters()
+        {
+            var resultExpect = "Son necesarios el .Cer y el .Key en formato B64";
+            var build = new BuildSettings();
+            AcceptReject acceptReject = new AcceptReject(build.Url, build.User, build.Password);
+            var response = acceptReject.AcceptByCSD(build.Cer, build.Key, build.Rfc, build.CerPassword, new AceptacionRechazoItem[] { new AceptacionRechazoItem() { uuid = ""} });
+            Assert.IsTrue(response.messageDetail.Contains((string)resultExpect));
+        }
+        [TestMethod]
+        public void AcceptRejectByRfcUuid()
+        {
+            var build = new BuildSettings();
+            AcceptReject acceptReject = new AcceptReject(build.Url, build.User, build.Password);
+            var response = acceptReject.AcceptByRfcUuid(build.Rfc, "01724196-ac5a-4735-b621-e3b42bcbb459", EnumAcceptReject.Aceptacion);
+            Assert.IsTrue(response.message != null);
+        }
+        [TestMethod]
+        public void AcceptRejectByCSD()
+        {
+            var build = new BuildSettings();
+            AcceptReject acceptReject = new AcceptReject(build.Url, build.User, build.Password);
+            var response = acceptReject.AcceptByCSD(build.Cer, build.Key, build.Rfc, build.CerPassword, new AceptacionRechazoItem[] { new AceptacionRechazoItem() { uuid = "01724196-ac5a-4735-b621-e3b42bcbb459", action = EnumAcceptReject.Aceptacion } });
+            Assert.IsTrue(response.message != null, response.message);
+        }
+        [TestMethod]
+        public void AcceptRejectByPfx()
+        {
+            var build = new BuildSettings();
+            AcceptReject acceptReject = new AcceptReject(build.Url, build.User, build.Password);
+            var response = acceptReject.AcceptByPFX(build.Pfx, build.Rfc, build.CerPassword, new AceptacionRechazoItem[] { new AceptacionRechazoItem() { uuid = "01724196-ac5a-4735-b621-e3b42bcbb459", action = EnumAcceptReject.Aceptacion } });
+            Assert.IsTrue(response.message != null, response.message);
+        }
+        [TestMethod]
+        public void AcceptRejectByXml()
+        {
+            var build = new BuildSettings();
+            AcceptReject acceptReject = new AcceptReject(build.Url, build.User, build.Password);
+            var response = acceptReject.AcceptByXML(build.Acuse, EnumAcceptReject.Aceptacion);
+            Assert.IsTrue(response.message != null, response.message);
+        }
+    }
+}
