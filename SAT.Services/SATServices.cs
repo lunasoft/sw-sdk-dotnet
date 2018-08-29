867cc2bd208d6b538b98f33d01c0076934f33fa1
alxcode <ing.gramos@outlook.com>
2018-08-28 22:59:53 -0500

    Add status cfdi

---
+++ SAT.Services/SATServices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace SAT.Services
+{
+    public class SATServices
+    {
+        private string _url;
+        public string Url
+        {
+            get { return _url; }
+        }
+
+        public SATServices(string url)
+        {
+            _url = url; ;
+        }
+
+        public BasicHttpBinding GetBinding()
+        {
+            var myBinding = new BasicHttpBinding();
+            myBinding.Security.Mode = Url.StartsWith("https") ? BasicHttpSecurityMode.Transport : BasicHttpSecurityMode.None;
+            myBinding.ReceiveTimeout = new TimeSpan(0, 1, 0);
+            myBinding.SendTimeout = new TimeSpan(0, 1, 0);
+            myBinding.OpenTimeout = new TimeSpan(0, 1, 0);
+            myBinding.CloseTimeout = new TimeSpan(0, 1, 0);
+            myBinding.MaxReceivedMessageSize = 2147483647;
+            myBinding.BypassProxyOnLocal = true;
+            myBinding.UseDefaultWebProxy = true;
+            return myBinding;
+        }
+    }
+}
