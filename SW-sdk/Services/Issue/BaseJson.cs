f0cdbbb71b2845c386e5e4e465b47dc730e503b4
Juan Gamez <juan.gamez@sw.com.mx>
2018-11-14 10:27:26 -0600

    Se añade soporte de timbrado por JSON.

---
+++ SW-sdk/Services/Issue/BaseJson.cs
@@ -0,0 +1,72 @@
+﻿using SW.Helpers;
+using SW.Services.Stamp;
+using System;
+using System.Text;
+
+namespace SW.Services.Issue
+{
+    public abstract class BaseStampJson : IssueService
+    {
+        private string _operation;
+        public BaseStampJson(string url, string user, string password, string operation, int proxyPort = 0, string proxy = null) : base(url, user, password, proxy, proxyPort)
+        {
+            _operation = operation;
+        }
+        public BaseStampJson(string url, string token, string operation, int proxyPort = 0, string proxy = null) : base(url, token, proxy, proxyPort)
+        {
+            _operation = operation;
+        }
+        public virtual StampResponseV1 TimbrarJsonV1(string json)
+        {
+            StampResponseHandlerV1 handler = new StampResponseHandlerV1();
+            try
+            {
+                var request = this.RequestStampJson(json, StampTypes.v1.ToString(), _operation);
+                return handler.GetResponse(request);
+            }
+            catch (Exception ex)
+            {
+                return handler.HandleException(ex);
+            }
+        }
+        public virtual StampResponseV2 TimbrarJsonV2(string json)
+        {
+            StampResponseHandlerV2 handler = new StampResponseHandlerV2();
+            try
+            {
+                var request = this.RequestStampJson(json, StampTypes.v2.ToString(), _operation);
+                return handler.GetResponse(request);
+            }
+            catch (Exception ex)
+            {
+                return handler.HandleException(ex);
+            }
+        }
+        public virtual StampResponseV3 TimbrarJsonV3(string json)
+        {
+            StampResponseHandlerV3 handler = new StampResponseHandlerV3();
+            try
+            {
+                var request = this.RequestStampJson(json, StampTypes.v3.ToString(), _operation);
+                return handler.GetResponse(request);
+            }
+            catch (Exception ex)
+            {
+                return handler.HandleException(ex);
+            }
+        }
+        public virtual StampResponseV4 TimbrarJsonV4(string json)
+        {
+            StampResponseHandlerV4 handler = new StampResponseHandlerV4();
+            try
+            {
+                var request = this.RequestStampJson(json, StampTypes.v4.ToString(), _operation);
+                return handler.GetResponse(request);
+            }
+            catch (Exception ex)
+            {
+                return handler.HandleException(ex);
+            }
+        }
+    }
+}
---
+++ Test_SW-sdk/Services/Issue/IssueJson_Test.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Test_SW.Helpers;
+using SW.Services.Issue;
+using SW.Services.Stamp;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json.Linq;
+
+namespace Test_SW.Services.Issue
+{
+    [TestClass]
+    public class IssueJson_Test
+    {
+        [TestMethod]
+        public void IssueJsonV1()
+        {
+            var build = new BuildSettings();
+            SW.Services.Issue.IssueJson issue = new SW.Services.Issue.IssueJson(build.Url, build.User, build.Password);
+            var json = GetJson(build);
+            var response = (StampResponseV1)issue.TimbrarJsonV1(json);
+            Assert.IsTrue(response.status == "success"
+                && !string.IsNullOrEmpty(response.data.tfd), "El resultado data.tfd viene vacio.");
+        }
+
+        [TestMethod]
+        public void IssueJsonV2()
+        {
+            var build = new BuildSettings();
+            SW.Services.Issue.IssueJson issue = new SW.Services.Issue.IssueJson(build.Url, build.User, build.Password);
+            var json = GetJson(build);
+            var response = (StampResponseV2)issue.TimbrarJsonV2(json);
+            Assert.IsTrue(response.status == "success"
+                && !string.IsNullOrEmpty(response.data.tfd), "El resultado data.tfd viene vacio.");
+        }
+
+        [TestMethod]
+        public void IssueJsonV3()
+        {
+            var build = new BuildSettings();
+            SW.Services.Issue.IssueJson issue = new SW.Services.Issue.IssueJson(build.Url, build.User, build.Password);
+            var json = GetJson(build);
+            var response = (StampResponseV3)issue.TimbrarJsonV3(json);
+            Assert.IsTrue(response.status == "success"
+                && !string.IsNullOrEmpty(response.data.cfdi), "El resultado data.tfd viene vacio.");
+        }
+
+        [TestMethod]
+        public void StampJsonV4byToken()
+        {
+            var build = new BuildSettings();
+            SW.Services.Issue.IssueJson issue = new SW.Services.Issue.IssueJson(build.Url, build.Token);
+            var json = GetJson(build);
+            var response = (StampResponseV4)issue.TimbrarJsonV4(json);
+            Assert.IsTrue(response.data != null, "El resultado data viene vacio.");
+            Assert.IsTrue(!string.IsNullOrEmpty(response.data.cfdi), "El resultado data.cfdi viene vacio.");
+            Assert.IsTrue(!string.IsNullOrEmpty(response.data.cadenaOriginalSAT), "El resultado data.cadenaOriginalSAT viene vacio.");
+            Assert.IsTrue(!string.IsNullOrEmpty(response.data.noCertificadoSAT), "El resultado data.noCertificadoSAT viene vacio.");
+            Assert.IsTrue(!string.IsNullOrEmpty(response.data.noCertificadoCFDI), "El resultado data.noCertificadoCFDI viene vacio.");
+            Assert.IsTrue(!string.IsNullOrEmpty(response.data.uuid), "El resultado data.uuid viene vacio.");
+            Assert.IsTrue(!string.IsNullOrEmpty(response.data.selloSAT), "El resultado data.selloSAT viene vacio.");
+            Assert.IsTrue(!string.IsNullOrEmpty(response.data.selloCFDI), "El resultado data.selloCFDI viene vacio.");
+            Assert.IsTrue(!string.IsNullOrEmpty(response.data.fechaTimbrado), "El resultado data.fechaTimbrado viene vacio.");
+            Assert.IsTrue(!string.IsNullOrEmpty(response.data.qrCode), "El resultado data.qrCode viene vacio.");
+        }
+
+        private string GetJson(BuildSettings build)
+        {
+            var file = Encoding.UTF8.GetString(File.ReadAllBytes("Resources/cfdi.json"));
+            var json = JObject.Parse(file);
+            json["Fecha"] = DateTime.Now.AddHours(-12).ToString("s");
+            return json.ToString();
+        }
+
+    }
+}
