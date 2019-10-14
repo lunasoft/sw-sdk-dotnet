4ff1e951cd1c87ee908f76e749b169ab9a5766ca
DanielIxiguez <edaniel.medina@outlook.com>
2019-10-14 18:51:47 -0500

    Se aumento la versión de la librería

--- Test_SW-sdk-45/Services/Taxpayers/Taxplayers_test.cs
+++ Test_SW-sdk-45/Services/Taxpayers/Taxplayers_test.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SW.Services.Taxpayer;
 using Test_SW.Helpers;
+using System.Diagnostics;
 
 namespace Test_SW_sdk_45.Services.TaxpayersService
 {
@@ -23,6 +24,22 @@ namespace Test_SW_sdk_45.Services.TaxpayersService
                 var build = new BuildSettings();
                 Taxpayer Taxpayer = new Taxpayer(build.Url, build.User, build.Password);
                 var response = Taxpayer.GetTaxpayer("ZNS1101105T3");
+                Debug.WriteLine(response.data.id);
+                Debug.WriteLine(response.data.rfc);
+                Debug.WriteLine(response.data.nombre_Contribuyente);
+                Debug.WriteLine(response.data.numero_fecha_oficio_global_contribuyentes_que_desvirtuaron);
+                Debug.WriteLine(response.data.numero_fecha_oficio_global_definitivos);
+                Debug.WriteLine(response.data.numero_fecha_oficio_global_sentencia_favorable);
+                Debug.WriteLine(response.data.numero_y_fecha_oficio_global_presuncion);
+                Debug.WriteLine(response.data.publicacion_DOF_definitivos);
+                Debug.WriteLine(response.data.publicacion_DOF_desvirtuados);
+                Debug.WriteLine(response.data.publicacion_DOF_presuntos);
+                Debug.WriteLine(response.data.publicacion_DOF_sentencia_favorable);
+                Debug.WriteLine(response.data.publicacion_pagina_SAT_definitivos);
+                Debug.WriteLine(response.data.publicacion_pagina_SAT_desvirtuados);
+                Debug.WriteLine(response.data.publicacion_pagina_SAT_presuntos);
+                Debug.WriteLine(response.data.publicacion_pagina_SAT_sentencia_favorable);
+                Debug.WriteLine(response.data.situacion_del_contribuyente);
                 Assert.IsTrue(response.status == "success", response.message);
             }
             [TestMethod]
