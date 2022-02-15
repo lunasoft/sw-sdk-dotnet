332efc0f0679c3054101f2f667faa587a339aa7b
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-02-15 15:49:42 -0600

    UT SignXML
    - Actualizar SignXml para poder sellar CFDI 4.0

--- Test_SW-sdk/Helpers/SignTools.cs
+++ Test_SW-sdk/Helpers/SignTools.cs
@@ -9,7 +9,7 @@ namespace Test_SW.Helpers
     public static class SignTools
     {
         static Random randomNumber = new Random(1);
-        public static string SigXml(string xml, byte[] pfx, string password)
+        public static string SigXml(string xml, byte[] pfx, string password, string version = "4.0")
         {
             xml = SW.Tools.Fiscal.RemoverCaracteresInvalidosXml(xml);
             XmlDocument doc = new XmlDocument();
@@ -17,7 +17,7 @@ namespace Test_SW.Helpers
             doc.DocumentElement.SetAttribute("Fecha", DateTime.Now.AddHours(-12).ToString("s"));
             doc.DocumentElement.SetAttribute("Folio", DateTime.Now.Ticks.ToString() + randomNumber.Next(100));
             xml = doc.OuterXml;
-            xml = SW.Tools.Sign.SellarCFDIv33(pfx, password, xml);
+            xml = version != "4.0" ? SW.Tools.Sign.SellarCFDIv33(pfx, password, xml) : SW.Tools.Sign.SellarCFDIv40(pfx, password, xml);
             return xml;
         }
     }
