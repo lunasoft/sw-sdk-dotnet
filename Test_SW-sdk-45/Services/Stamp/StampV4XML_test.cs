ca48b9260303e4bcbd5048be6ee6db2732f0e92e
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-03-17 17:24:40 -0600

    StampV4
    - Agregar metodo para timbrar con customId con respuesta v4
    - Agregar UT para timbrado de customId con respuesta v4

--- Test_SW-sdk-45/Services/Stamp/StampV4XML_test.cs
+++ Test_SW-sdk-45/Services/Stamp/StampV4XML_test.cs
@@ -12,7 +12,7 @@ namespace Test_SW.Services.Stamp_Test
     public class StampV4XML_Test
     {
         [TestMethod]
-        public void Stamp_Test_StampV4XMLV4_SameCustomID_byToken_Ok()
+        public void Stamp_Test_StampV4XMLV2_SameCustomID_byToken_Ok()
         {
             string CustomId = Guid.NewGuid().ToString();
             var build = new BuildSettings();
@@ -26,9 +26,23 @@ namespace Test_SW.Services.Stamp_Test
             response = (StampResponseV2) stamp.TimbrarV2(xml, null, CustomId);
             Assert.IsTrue(response.status == "error" && response.message == "CFDI3307 - Timbre duplicado. El customId proporcionado está duplicado.");
         }
-
         [TestMethod]
-        public void Stamp_Test_StampV4XMLV4_SameCustomID_byToken_NoExistURLXML()
+        public void Stamp_Test_StampV4XMLV4_SameCustomID_byToken_Ok()
+        {
+            string CustomId = Guid.NewGuid().ToString();
+            var build = new BuildSettings();
+            StampV4XML stamp = new StampV4XML(build.Url, build.UrlApi, build.Token);
+            var xml = GetXml(build);
+            var response = stamp.TimbrarV4(xml, null, CustomId);
+            Assert.IsTrue(response.data != null, "El resultado data viene vacio.");
+            Assert.IsTrue(!string.IsNullOrEmpty(response.data.cfdi), "El resultado data.cfdi viene vacio.");
+            System.Threading.Thread.Sleep(5000);
+            xml = GetXml(build);
+            response = stamp.TimbrarV4(xml, null, CustomId);
+            Assert.IsTrue(response.status == "error" && response.message == "CFDI3307 - Timbre duplicado. El customId proporcionado está duplicado.");
+        }
+        [TestMethod]
+        public void Stamp_Test_StampV4XMLV2_SameCustomID_byToken_NoExistURLXML()
         {
                 string CustomId = Guid.NewGuid().ToString();
                 var build = new BuildSettings();
@@ -42,9 +56,23 @@ namespace Test_SW.Services.Stamp_Test
                 response = (StampResponseV2)stamp.TimbrarV2(xml, null, CustomId);
                 Assert.IsTrue(response.status == "error" && response.message == "No es posible obtener el url para descargar el XML");
         }
-
         [TestMethod]
-        public void Stamp_Test_StampV4XMLV4_DifCustomID_byToken()
+        public void Stamp_Test_StampV4XMLV4_SameCustomID_byToken_NoExistURLXML()
+        {
+            string CustomId = Guid.NewGuid().ToString();
+            var build = new BuildSettings();
+            StampV4XML stamp = new StampV4XML(build.Url, build.UrlApi, build.Token);
+            var xml = GetXml(build);
+            var response = stamp.TimbrarV4(xml, null, CustomId);
+            Assert.IsTrue(response.data != null, "El resultado data viene vacio.");
+            Assert.IsTrue(!string.IsNullOrEmpty(response.data.cfdi), "El resultado data.cfdi viene vacio.");
+
+            xml = GetXml(build);
+            response = stamp.TimbrarV4(xml, null, CustomId);
+            Assert.IsTrue(response.status == "error" && response.message == "No es posible obtener el url para descargar el XML");
+        }
+        [TestMethod]
+        public void Stamp_Test_StampV4XMLV2_DifCustomID_byToken()
         {
             string CustomIdfirstRequest = Guid.NewGuid().ToString();
             string CustomIdSecondRequest = Guid.NewGuid().ToString();
@@ -59,7 +87,22 @@ namespace Test_SW.Services.Stamp_Test
 
             Assert.IsTrue(response.status == "error" && response.message == "307. El comprobante contiene un timbre previo.");
         }
+        [TestMethod]
+        public void Stamp_Test_StampV4XMLV4_DifCustomID_byToken()
+        {
+            string CustomIdfirstRequest = Guid.NewGuid().ToString();
+            string CustomIdSecondRequest = Guid.NewGuid().ToString();
+            var build = new BuildSettings();
+            BaseStampV4XML stamp = new StampV4XML(build.Url, build.UrlApi, build.Token);
+            var xml = GetXml(build);
+            var response = stamp.TimbrarV4(xml, null, CustomIdfirstRequest);
+            Assert.IsTrue(response.data != null, "El resultado data viene vacio.");
 
+            Assert.IsTrue(!string.IsNullOrEmpty(response.data.cfdi), "El resultado data.cfdi viene vacio.");
+            response = stamp.TimbrarV4(xml, null, CustomIdSecondRequest);
+
+            Assert.IsTrue(response.status == "error" && response.message == "307. El comprobante contiene un timbre previo.");
+        }
         private string GetXml(BuildSettings build)
         {
             var xml = Encoding.UTF8.GetString(File.ReadAllBytes("Resources/file.xml"));
