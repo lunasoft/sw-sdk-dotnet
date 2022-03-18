519029d5d967d4fe265c7b9abaa3039194e258e3
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-03-17 18:23:50 -0600

    Actualizar UT

--- Test_SW-sdk-45/Services/Stamp/StampV4XML_test.cs
+++ Test_SW-sdk-45/Services/Stamp/StampV4XML_test.cs
@@ -39,6 +39,7 @@ namespace Test_SW.Services.Stamp_Test
             System.Threading.Thread.Sleep(5000);
             xml = GetXml(build);
             response = stamp.TimbrarV4(xml, null, CustomId);
+            ValidateResponseV4(response);
             Assert.IsTrue(response.status == "error" && response.message == "CFDI3307 - Timbre duplicado. El customId proporcionado está duplicado.");
         }
         [TestMethod]
@@ -51,7 +52,6 @@ namespace Test_SW.Services.Stamp_Test
                 var response = (StampResponseV2)stamp.TimbrarV2(xml, null, CustomId);
                 Assert.IsTrue(response.data != null, "El resultado data viene vacio.");
                 Assert.IsTrue(!string.IsNullOrEmpty(response.data.cfdi), "El resultado data.cfdi viene vacio.");
-
                 xml = GetXml(build);
                 response = (StampResponseV2)stamp.TimbrarV2(xml, null, CustomId);
                 Assert.IsTrue(response.status == "error" && response.message == "No es posible obtener el url para descargar el XML");
@@ -66,7 +66,6 @@ namespace Test_SW.Services.Stamp_Test
             var response = stamp.TimbrarV4(xml, null, CustomId);
             Assert.IsTrue(response.data != null, "El resultado data viene vacio.");
             Assert.IsTrue(!string.IsNullOrEmpty(response.data.cfdi), "El resultado data.cfdi viene vacio.");
-
             xml = GetXml(build);
             response = stamp.TimbrarV4(xml, null, CustomId);
             Assert.IsTrue(response.status == "error" && response.message == "No es posible obtener el url para descargar el XML");
@@ -81,10 +80,8 @@ namespace Test_SW.Services.Stamp_Test
             var xml = GetXml(build);
             var response = (StampResponseV2) stamp.TimbrarV2(xml, null, CustomIdfirstRequest);
             Assert.IsTrue(response.data != null, "El resultado data viene vacio.");
-
             Assert.IsTrue(!string.IsNullOrEmpty(response.data.cfdi), "El resultado data.cfdi viene vacio.");
             response = (StampResponseV2) stamp.TimbrarV2(xml, null, CustomIdSecondRequest);
-
             Assert.IsTrue(response.status == "error" && response.message == "307. El comprobante contiene un timbre previo.");
         }
         [TestMethod]
@@ -97,10 +94,9 @@ namespace Test_SW.Services.Stamp_Test
             var xml = GetXml(build);
             var response = stamp.TimbrarV4(xml, null, CustomIdfirstRequest);
             Assert.IsTrue(response.data != null, "El resultado data viene vacio.");
-
             Assert.IsTrue(!string.IsNullOrEmpty(response.data.cfdi), "El resultado data.cfdi viene vacio.");
             response = stamp.TimbrarV4(xml, null, CustomIdSecondRequest);
-
+            ValidateResponseV4(response);
             Assert.IsTrue(response.status == "error" && response.message == "307. El comprobante contiene un timbre previo.");
         }
         private string GetXml(BuildSettings build)
@@ -108,6 +104,18 @@ namespace Test_SW.Services.Stamp_Test
             var xml = Encoding.UTF8.GetString(File.ReadAllBytes("Resources/file.xml"));
             xml = SignTools.SigXml(xml, Convert.FromBase64String(build.Pfx), build.CerPassword);
             return xml;
+        }
+        private static void ValidateResponseV4(StampResponseV4 response)
+        {
+            Assert.IsTrue(!String.IsNullOrEmpty(response.data.cadenaOriginalSAT));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.data.cfdi));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.data.fechaTimbrado));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.data.noCertificadoCFDI));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.data.noCertificadoSAT));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.data.qrCode));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.data.selloCFDI));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.data.selloSAT));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.data.uuid));
         }
     }
 }
