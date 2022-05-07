8923396d7ba92285de7ba0bb7c3aaa23610442ef
SwAeyrton <aeyrton170795@gmail.com>
2022-05-07 13:51:56 -0500

    Fix StampV4XML
    - Se corrige un error que lanzaba una excepcion cuando la respuesta era un status error y el xml tenia un error.
    - Se agrega xml de error y UT para los escenarios donde se lanzaba la esxcepcion.
    - Se corrige un error con un resource  las UT de Cancelacion.

--- Test_SW-sdk-45/Helpers/SignTools.cs
+++ Test_SW-sdk-45/Helpers/SignTools.cs
@@ -8,12 +8,15 @@ namespace Test_SW.Helpers
     public static class SignTools
     {
         static Random randomNumber = new Random(1);
-        public static string SigXml(string xml, byte[] pfx, string password)
+        public static string SigXml(string xml, byte[] pfx, string password, bool setDate = true)
         {
             xml = Fiscal.RemoverCaracteresInvalidosXml(xml);
             XmlDocument doc = new XmlDocument();
             doc.LoadXml(xml);
-            doc.DocumentElement.SetAttribute("Fecha", DateTime.Now.AddHours(-12).ToString("s"));
+            if (setDate)
+            {
+                doc.DocumentElement.SetAttribute("Fecha", DateTime.Now.AddHours(-12).ToString("s"));
+            }
             doc.DocumentElement.SetAttribute("Folio", DateTime.Now.Ticks.ToString() + randomNumber.Next(100));
             xml = doc.OuterXml;
             xml = Sign.SellarCFDIv33(pfx, password, xml);
