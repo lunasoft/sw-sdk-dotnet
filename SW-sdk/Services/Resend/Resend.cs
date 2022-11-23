84587da01f32acb8d2c4bc8ba678125cff07e7ec
FernandoCarrilloSW <fernando.carrillo@sw.com.mx>
2022-11-23 13:48:26 -0600

    Se crea el servicio de Resend Email -Se crean instancias publicas de la clase Resend -Se crea la clase Resend -Se crea la clase Request -Se crea la clase Response -Se crea la clase ResponseHandler

---
+++ SW-sdk/Services/Resend/Resend.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SW.Services.Resend
+{
+    public class Resend : BaseResend
+    {
+        /// <summary>
+        /// Crear una instancia de la clase Resend.
+        /// </summary>
+        /// <param name="urlApi">URL API.</param>
+        /// <param name="url">URL Services.</param>
+        /// <param name="user">Email del usuario.</param>
+        /// <param name="password">Contraseña.</param>
+        /// <param name="proxyPort">Puerto proxy.</param>
+        /// <param name="proxy">Proxy.</param>
+        public Resend(string url, string urlApi, string user, string password, int proxyPort = 0, string proxy = null) : base(url, urlApi, user, password, proxy, proxyPort)
+        {
+        }
+        /// <summary>
+        /// Crear una instancia de la clase Resend.
+        /// </summary>
+        /// <param name="urlApi">URL API.</param>
+        /// <param name="token">Token de autenticacion.</param>
+        /// <param name="proxyPort">Puerto proxy.</param>
+        /// <param name="proxy">Proxy.</param>
+        public Resend(string urlApi, string token, int proxyPort = 0, string proxy = null) : base(urlApi, token, proxy, proxyPort)
+        {
+        }
+    }
+}
---
+++ SW-sdk/Services/Resend/ResendService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace SW.Services.Resend
+{
+    public abstract class ResendService : Services
+    {
+        private string _operation;
+        private string _apiUrl;
+        protected ResendService(string urlApi, string url, string user, string password, string proxy, int proxyPort) : base(url, user, password, proxy, proxyPort)
+        {
+            _apiUrl = urlApi;
+        }
+        protected ResendService(string urlApi, string token, string proxy, int proxyPort) : base(urlApi, token, proxy, proxyPort)
+        {
+            _apiUrl = urlApi;
+        }
+
+        internal virtual HttpWebRequest RequestResend(Guid uuid, string email)
+        {
+            this.SetupRequest();
+            var request = (HttpWebRequest)WebRequest.Create(string.Format("{0}/comprobante/resendemail",_apiUrl));
+            request.ContentType = "application/json";
+            request.Method = WebRequestMethods.Http.Post;
+            request.Headers.Add(HttpRequestHeader.Authorization.ToString(), "bearer " + this.Token);
+            Helpers.RequestHelper.SetupProxy(this.Proxy, this.ProxyPort, ref request);
+            var body = Newtonsoft.Json.JsonConvert.SerializeObject(new ResendRequest()
+            {
+                uuid = uuid,
+                to = email
+            });
+            byte[] streamLenght = Encoding.UTF8.GetBytes(body);
+            request.ContentLength = streamLenght.Length;
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(body);
+                streamWriter.Flush();
+            }
+            return request;
+        }
+    }
+}
