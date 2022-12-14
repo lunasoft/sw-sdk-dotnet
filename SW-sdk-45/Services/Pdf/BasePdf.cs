264af1d385acd7d7d3e3741520aa974e231a04cd
SW-2017-04\Soporte2 <marifer.mares@sw.com.mx>
2022-12-14 13:18:35 -0600

    Se añade documentacion y una condicional para modificar status=="success"

--- SW-sdk-45/Services/Pdf/BasePdf.cs
+++ SW-sdk-45/Services/Pdf/BasePdf.cs
@@ -7,15 +7,38 @@ namespace SW.Services.Pdf
 {
     public abstract class BasePdf : PdfService
     {
-       
+        /// <summary>
+        /// Crear una instancia para PdfService heredado de Services
+        /// </summary>
+        /// <param name="urlApi">Url de la API</param>
+        /// <param name="token">Token de la cuenta del usuario</param>
+        /// <param name="proxy">Proxy </param>
+        /// <param name="proxyPort">Puerto proxy</param>
         public BasePdf(string urlApi, string token, string proxy, int proxyPort) : base( urlApi,token, proxy, proxyPort)
         {
         }
+        /// <summary>
+        /// Crear una instancia para BasePdf heredado de PdfService
+        /// </summary>
+        /// <param name="urlApi">Url de la API</param>
+        /// <param name="url">Url de Services</param>
+        /// <param name="user">Correo del usuario</param>
+        /// <param name="password">Contraseña del usuario</param>
+        /// <param name="proxy">Proxy </param>
+        /// <param name="proxyPort">Puerto proxy</param>
         public BasePdf(string urlApi, string url, string user, string password, string proxy, int proxyPort) : base(urlApi, url, user, password, proxy, proxyPort)
         {
         }
 
-
+        /// <summary>
+        /// Servicio para generar PDF con plantillas genericas.
+        /// </summary>
+        /// <param name="xml">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
+        /// <param name="logo">El logotipo en base 64 .</param>
+        /// <param name="templateId">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
+        /// <param name="ObservacionesAdicionales">Informacion extra </param>
+        /// <param name="isB64">Booleano que muestra si es b64 o no</param>
+        /// <returns>PdfResponse</returns>
         public virtual PdfResponse GenerarPdf(string xml, string logo,TemplatesId templateId, Dictionary<string, string> ObservacionesAdicionales = null, bool isB64 = false)
         {
             PdfResponseHandler handler = new PdfResponseHandler();
@@ -41,6 +64,15 @@ namespace SW.Services.Pdf
             }
 
         }
+        /// <summary>
+        /// Servicio para generar PDF con plantillas personalizadas.
+        /// </summary>
+        /// <param name="xml">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
+        /// <param name="logo">El logotipo en base 64 .</param>
+        /// <param name="templateId">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
+        /// <param name="ObservacionesAdicionales">Informacion extra </param>
+        /// <param name="isB64">Booleano que muestra si es b64 o no</param>
+        /// <returns>PdfResponse</returns>
         public virtual PdfResponse GenerarPdf(string xml, string logo, string templateId, Dictionary<string, string> ObservacionesAdicionales = null, bool isB64 = false)
         {
             PdfResponseHandler handler = new PdfResponseHandler();
@@ -66,6 +98,11 @@ namespace SW.Services.Pdf
             }
 
         }
+        /// <summary>
+        /// Servicio para regenerar PDF de un comprobante previamente timbrado.
+        /// </summary>
+        /// <param name="uuid">Folio fiscal del comprobante.</param>
+        /// <returns>PdfResponse</returns>
         public virtual PdfResponse RegenerarPdf(Guid uuid)
         {
             PdfResponseHandler handler = new PdfResponseHandler();
@@ -73,7 +110,9 @@ namespace SW.Services.Pdf
             {
                 new Validation(Url, User, Password, Token).ValidateHeaderParameters();
                 var request = this.RequestRegeneratePdf(uuid);
-                return handler.GetResponseRequest(request);
+                var response = handler.GetResponseRequest(request);
+                if (response.message == "Solicitud se proceso correctamente.") { response.status = "success"; }
+                return response;
             }
             catch (Exception e)
             {
--- SW-sdk/Services/Pdf/BasePdf.cs
+++ SW-sdk/Services/Pdf/BasePdf.cs
@@ -8,14 +8,37 @@ namespace SW.Services.Pdf
 {
     public abstract class BasePdf : PdfService
     {
-       
+        /// <summary>
+        /// Crear una instancia para PdfService heredado de Services
+        /// </summary>
+        /// <param name="urlApi">Url de la API</param>
+        /// <param name="token">Token de la cuenta del usuario</param>
+        /// <param name="proxy">Proxy </param>
+        /// <param name="proxyPort">Puerto proxy</param>
         public BasePdf(string urlApi, string token, string proxy, int proxyPort) : base( urlApi,token, proxy, proxyPort)
         {
         }
+        /// <summary>
+        /// Crear una instancia para BasePdf heredado de PdfService
+        /// </summary>
+        /// <param name="urlApi">Url de la API</param>
+        /// <param name="url">Url de Services</param>
+        /// <param name="user">Correo del usuario</param>
+        /// <param name="password">Contraseña del usuario</param>
+        /// <param name="proxy">Proxy </param>
+        /// <param name="proxyPort">Puerto proxy</param>
         public BasePdf(string urlApi, string url, string user, string password, string proxy, int proxyPort) : base(urlApi, url, user, password, proxy, proxyPort)
         {
         }
-
+        /// <summary>
+        /// Servicio para generar PDF con plantillas genericas.
+        /// </summary>
+        /// <param name="xml">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
+        /// <param name="logo">El logotipo en base 64 .</param>
+        /// <param name="templateId">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
+        /// <param name="ObservacionesAdicionales">Informacion extra </param>
+        /// <param name="isB64">Booleano que muestra si es b64 o no</param>
+        /// <returns>PdfResponse</returns>
 
         public virtual PdfResponse GenerarPdf(string xml, string logo,TemplatesId templateId, Dictionary<string, string> ObservacionesAdicionales = null, bool isB64 = false)
         {
@@ -42,6 +65,16 @@ namespace SW.Services.Pdf
             }
 
         }
+       
+        /// <summary>
+        /// Servicio para generar PDF con plantillas personalizadas.
+        /// </summary>
+        /// <param name="xml">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
+        /// <param name="logo">El logotipo en base 64 .</param>
+        /// <param name="templateId">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
+        /// <param name="ObservacionesAdicionales">Informacion extra </param>
+        /// <param name="isB64">Booleano que muestra si es b64 o no</param>
+        /// <returns>PdfResponse</returns>
         public virtual PdfResponse GenerarPdf(string xml, string logo, string templateId, Dictionary<string, string> ObservacionesAdicionales = null, bool isB64 = false)
         {
             PdfResponseHandler handler = new PdfResponseHandler();
@@ -67,13 +100,21 @@ namespace SW.Services.Pdf
             }
 
         }
+       
+        /// <summary>
+        /// Servicio para regenerar PDF de un comprobante previamente timbrado.
+        /// </summary>
+        /// <param name="uuid">Folio fiscal del comprobante.</param>
+        /// <returns>PdfResponse</returns>
         public virtual PdfResponse RegenerarPdf(Guid uuid) {
             PdfResponseHandler handler = new PdfResponseHandler();
             try
             {
                 new Validation(Url, User, Password, Token).ValidateHeaderParameters();
                 var request = this.RequestRegeneratePdf(uuid);
-                return handler.GetResponse(request);
+                var response= handler.GetResponse(request);
+                if (response.message == "Solicitud se proceso correctamente.") { response.status = "success"; }
+                return response;
             }
             catch (Exception e)
             {
