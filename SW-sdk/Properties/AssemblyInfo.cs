d51f1ecf7f749a5bd79eef7ed22dbc1bc42f66ce
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2023-03-03 13:40:36 -0600

    Update version

--- SW-sdk/Properties/AssemblyInfo.cs
+++ SW-sdk/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.19.2")]
-[assembly: AssemblyFileVersion("0.0.19.1")]
+[assembly: AssemblyFileVersion("0.0.19.2")]
 
 
