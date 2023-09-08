cbf20b6272d6f04de4bc7f1b44a41c3c2e676a9c
Sebastian Vidal <sebastian.vidal@sw.com.mx>
2023-09-07 18:39:42 -0600

    Update environment variables

--- Test_SW-sdk-45/Services/Pdf/Pdf_Test.cs
+++ Test_SW-sdk-45/Services/Pdf/Pdf_Test.cs
@@ -138,7 +138,7 @@ namespace Pdf_Test.Services.Pdf_Tests
         {
             var build = new BuildSettings();
             Pdf regeneratePdf = new Pdf(build.UrlApi, build.Url, build.User, build.Password);
-            var response = regeneratePdf.RegenerarPdf(new Guid("a519df30-f591-4c33-bbb9-277df20597e9"));
+            var response = regeneratePdf.RegenerarPdf(new Guid("9529824a-24e5-4ea6-900c-476ed11f0ea5"));
             Assert.IsTrue(response.status == "success");
             Assert.IsTrue(response.message == "Solicitud se proceso correctamente.");
         }
@@ -148,7 +148,7 @@ namespace Pdf_Test.Services.Pdf_Tests
         {
             var build = new BuildSettings();
             Pdf regeneratePdf = new Pdf(build.UrlApi, build.Token);
-            var response = regeneratePdf.RegenerarPdf(new Guid("3f7477b2-6cca-45af-95f9-e4ca3d8fe68d"));
+            var response = regeneratePdf.RegenerarPdf(new Guid("9529824a-24e5-4ea6-900c-476ed11f0ea5"));
             Assert.IsTrue(response.status == "success");
             Assert.IsTrue(response.message == "Solicitud se proceso correctamente.");
         }
