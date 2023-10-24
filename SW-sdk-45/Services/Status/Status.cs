496ba860226dbd5da5994d7f3c95ce78b87c2b19
Sebastian Vidal <sebastian.vidal@sw.com.mx>
2023-10-24 13:54:15 -0600

    Add sello to GetStatusCFDI services

--- SW-sdk-45/Services/Status/Status.cs
+++ SW-sdk-45/Services/Status/Status.cs
@@ -12,13 +12,13 @@ namespace SW.Services.Status
         public Status(string url) : base(url)
         {
         }
-        internal override Acuse StatusRequest(string rfcEmisor, string rfcReceptor, string total, string uuid)
+        internal override Acuse StatusRequest(string rfcEmisor, string rfcReceptor, string total, string uuid, string sello)
         {
-            return this.RequestStatus(rfcEmisor, rfcReceptor, total, uuid);
+            return this.RequestStatus(rfcEmisor, rfcReceptor, total, uuid, sello);
         }
-        public Acuse GetStatusCFDI(string rfcEmisor, string rfcReceptor, string Total, string uuid)
+        public Acuse GetStatusCFDI(string rfcEmisor, string rfcReceptor, string Total, string uuid, string sello)
         {
-            return StatusRequest(rfcEmisor, rfcReceptor, Total, uuid);
+            return StatusRequest(rfcEmisor, rfcReceptor, Total, uuid, sello);
         }
     }
 }
--- SW-sdk/Services/Status/Status.cs
+++ SW-sdk/Services/Status/Status.cs
@@ -8,14 +8,14 @@ namespace SW.Services.Status
         {
         }
 
-        internal override Acuse StatusRequest(string rfcEmisor, string rfcReceptor, string total, string uuid)
+        internal override Acuse StatusRequest(string rfcEmisor, string rfcReceptor, string total, string uuid, string sello)
         {
-            return this.RequestStatus(rfcEmisor, rfcReceptor, total, uuid);
+            return this.RequestStatus(rfcEmisor, rfcReceptor, total, uuid, sello);
         }
        
-        public Acuse GetStatusCFDI(string rfcEmisor, string rfcReceptor, string Total, string uuid)
+        public Acuse GetStatusCFDI(string rfcEmisor, string rfcReceptor, string Total, string uuid, string sello)
         {
-            return StatusRequest(rfcEmisor, rfcReceptor, Total, uuid);
+            return StatusRequest(rfcEmisor, rfcReceptor, Total, uuid, sello);
         }
     }
 }
