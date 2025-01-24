37504dba4b3afd8410ca6686b42b820a5306b037
Marifer Mares <marifer.mares@sw.com.mx>
2025-01-23 19:57:53 -0600

    add request body

--- SW-sdk/Services/Pdf/PdfService.cs
+++ SW-sdk/Services/Pdf/PdfService.cs
@@ -4,7 +4,6 @@ using SW.Helpers;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
 
@@ -12,80 +11,113 @@ namespace SW.Services.Pdf
 {
     public abstract class PdfService : Services
     {
-        private string _operation;
-        private string _apiUrl;
+        private readonly string _apiUrl;
+
         /// <summary>
         /// Crear una instancia para PdfService heredado de Services
         /// </summary>
-        /// <param name="urlApi">Url de la API</param>
-        /// <param name="url">Url de Services</param>
+        /// <param name="urlApi">URL de la API</param>
+        /// <param name="url">URL de Services</param>
         /// <param name="user">Correo del usuario</param>
         /// <param name="password">Contraseña del usuario</param>
-        /// <param name="proxy">Proxy </param>
+        /// <param name="proxy">Proxy</param>
         /// <param name="proxyPort">Puerto proxy</param>
-        protected PdfService(string urlApi, string url, string user, string password, string proxy, int proxyPort) : base(url, user, password, proxy, proxyPort)
+        protected PdfService(string urlApi, string url, string user, string password, string proxy, int proxyPort)
+            : base(url, user, password, proxy, proxyPort)
         {
             _apiUrl = urlApi;
         }
+
         /// <summary>
         /// Crear una instancia para PdfService heredado de Services
         /// </summary>
-        /// <param name="urlApi">Url de la API</param>
+        /// <param name="urlApi">URL de la API</param>
         /// <param name="token">Token de la cuenta del usuario</param>
-        /// <param name="proxy">Proxy </param>
+        /// <param name="proxy">Proxy</param>
         /// <param name="proxyPort">Puerto proxy</param>
-        protected PdfService(string urlApi, string token, string proxy, int proxyPort) : base(urlApi, token, proxy, proxyPort)
+        protected PdfService(string urlApi, string token, string proxy, int proxyPort)
+            : base(urlApi, token, proxy, proxyPort)
         {
             _apiUrl = urlApi;
         }
+
         /// <summary>
-        /// Peticion post para generar PDF de un comprobante previamente timbrado.
+        /// Crea y configura una solicitud HTTP.
         /// </summary>
-        /// <param name="xml">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
-        /// <param name="logo">El logotipo en base 64 .</param>
-        /// <param name="TemplateId">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
-        /// <param name="ObservacionesAdicionales">Informacion extra </param>
-        /// <returns>request</returns>
-        internal virtual HttpWebRequest RequestPdf(string xml, string logo, string TemplateId, Dictionary<string, string> ObservacionesAdicionales = null)
+        /// <param name="endpoint">Endpoint de la API.</param>
+        /// <returns>Una instancia configurada de <see cref="HttpWebRequest"/>.</returns>
+        private HttpWebRequest CreateHttpRequest(string endpoint)
         {
-            this.SetupRequest();
-            var request = (HttpWebRequest)WebRequest.Create(_apiUrl + string.Format("/pdf/v1/api/GeneratePdf"));
+            var request = (HttpWebRequest)WebRequest.Create(_apiUrl + endpoint);
             request.ContentType = "application/json";
             request.Method = WebRequestMethods.Http.Post;
-            request.Headers.Add(HttpRequestHeader.Authorization.ToString(), "bearer " + this.Token);
-            Helpers.RequestHelper.SetupProxy(this.Proxy, this.ProxyPort, ref request);
-            var body = Newtonsoft.Json.JsonConvert.SerializeObject(new PdfRequest()
-            {
-                xmlContent= xml,
-                logo = logo,
-                extras = ObservacionesAdicionales,
-                templateId = TemplateId
-            });
-            byte[] streamLenght = Encoding.UTF8.GetBytes(body);
-            request.ContentLength = streamLenght.Length;
+            request.Headers.Add(HttpRequestHeader.Authorization, $"Bearer "+this.Token);
+            RequestHelper.SetupProxy(Proxy, ProxyPort, ref request);
+            return request;
+        }
+
+        /// <summary>
+        /// Envía los datos en formato JSON al cuerpo de la solicitud.
+        /// </summary>
+        /// <param name="request">Instancia de la solicitud HTTP.</param>
+        /// <param name="body">Cuerpo de la solicitud en formato JSON.</param>
+        private void WriteRequestBody(HttpWebRequest request, string body)
+        {
+            byte[] bodyBytes = Encoding.UTF8.GetBytes(body);
+            request.ContentLength = bodyBytes.Length;
+
             using (var streamWriter = new StreamWriter(request.GetRequestStream()))
             {
                 streamWriter.Write(body);
-                streamWriter.Flush();
-                streamWriter.Close();
             }
+        }
+
+        /// <summary>
+        /// Petición POST para generar PDF de un comprobante previamente timbrado.
+        /// </summary>
+        /// <param name="xml">XML con formato UTF-8 del comprobante.</param>
+        /// <param name="logo">El logotipo en base64.</param>
+        /// <param name="templateId">Identificador de la plantilla.</param>
+        /// <param name="extras">Información adicional.</param>
+        /// <returns>Una instancia configurada de <see cref="HttpWebRequest"/>.</returns>
+        internal virtual HttpWebRequest RequestPdf(string xml, string logo=null, string templateId = null, Dictionary<string, string> extras = null)
+        {
+            SetupRequest();
+
+            var request = CreateHttpRequest("/pdf/v1/api/GeneratePdf");
+            var body = JsonConvert.SerializeObject(new PdfRequest
+            {
+                xmlContent = xml,
+                logo = logo,
+                extras = extras,
+                templateId = templateId
+            });
+
+            WriteRequestBody(request, body);
             return request;
         }
+
         /// <summary>
-        /// Peticion post para regenerar PDF de un comprobante previamente timbrado.
+        /// Petición POST para regenerar PDF de un comprobante previamente timbrado.
         /// </summary>
         /// <param name="uuid">Folio fiscal del comprobante.</param>
-        /// <returns>request</returns>
-        internal virtual HttpWebRequest RequestRegeneratePdf(Guid uuid)
+        /// <param name="logo">El logotipo en base64.</param>
+        /// <param name="templateId">Identificador de la plantilla.</param>
+        /// <param name="extras">Información adicional.</param>
+        /// <returns>Una instancia configurada de <see cref="HttpWebRequest"/>.</returns>
+        internal virtual HttpWebRequest RequestRegeneratePdf(Guid uuid, string logo=null, string templateId = null, Dictionary<string, string> extras = null)
         {
-            this.SetupRequest();
-            string path = $"/pdf/v1/api/RegeneratePdf/{uuid}";
-            var request = (HttpWebRequest)WebRequest.Create(this._apiUrl + path);
-            request.ContentType = "application/json";
-            request.ContentLength = 0;
-            request.Method = WebRequestMethods.Http.Post;
-            request.Headers.Add(HttpRequestHeader.Authorization.ToString(), "bearer " + this.Token);
-            Helpers.RequestHelper.SetupProxy(this.Proxy, this.ProxyPort, ref request);
+            SetupRequest();
+
+            var request = CreateHttpRequest($"/pdf/v1/api/RegeneratePdf/{uuid}");
+            var body = JsonConvert.SerializeObject(new PdfRequest
+            {
+                logo = logo,
+                extras = extras,
+                templateId = templateId
+            });
+
+            WriteRequestBody(request, body);
             return request;
         }
     }
