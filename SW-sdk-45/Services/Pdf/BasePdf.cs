35329e889a8ae8b9ffbef8f23995e99029aa1e55
Maria Fernanda Lopez Mares <108951979+marifersw@users.noreply.github.com>
2025-01-27 09:20:42 -0600

    Feature/send body regenerate pdf (#155)
    * update version
    
    * add extra parameter
    
    * add request body
    
    * add ut regenerate pdf with body
    
    * update readme

--- SW-sdk-45/Services/Pdf/BasePdf.cs
+++ SW-sdk-45/Services/Pdf/BasePdf.cs
@@ -8,117 +8,113 @@ namespace SW.Services.Pdf
     public abstract class BasePdf : PdfService
     {
         /// <summary>
-        /// Crear una instancia para PdfService heredado de Services
+        /// Crea una instancia de PdfService heredado de Services.
         /// </summary>
-        /// <param name="urlApi">Url de la API</param>
-        /// <param name="token">Token de la cuenta del usuario</param>
-        /// <param name="proxy">Proxy </param>
-        /// <param name="proxyPort">Puerto proxy</param>
-        public BasePdf(string urlApi, string token, string proxy, int proxyPort) : base( urlApi,token, proxy, proxyPort)
-        {
-        }
-        /// <summary>
-        /// Crear una instancia para BasePdf heredado de PdfService
-        /// </summary>
-        /// <param name="urlApi">Url de la API</param>
-        /// <param name="url">Url de Services</param>
-        /// <param name="user">Correo del usuario</param>
-        /// <param name="password">Contraseña del usuario</param>
-        /// <param name="proxy">Proxy </param>
-        /// <param name="proxyPort">Puerto proxy</param>
-        public BasePdf(string urlApi, string url, string user, string password, string proxy, int proxyPort) : base(urlApi, url, user, password, proxy, proxyPort)
+        /// <param name="urlApi">URL de la API.</param>
+        /// <param name="token">Token de la cuenta del usuario.</param>
+        /// <param name="proxy">Dirección del proxy.</param>
+        /// <param name="proxyPort">Puerto del proxy.</param>
+        public BasePdf(string urlApi, string token, string proxy, int proxyPort)
+            : base(urlApi, token, proxy, proxyPort)
         {
         }
 
         /// <summary>
-        /// Servicio para generar PDF con plantillas genericas.
+        /// Crea una instancia de BasePdf heredado de PdfService.
         /// </summary>
-        /// <param name="xml">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
-        /// <param name="logo">El logotipo en base 64 .</param>
-        /// <param name="templateId">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
-        /// <param name="ObservacionesAdicionales">Informacion extra </param>
-        /// <param name="isB64">Booleano que muestra si es b64 o no</param>
-        /// <returns>PdfResponse</returns>
-        public virtual PdfResponse GenerarPdf(string xml, string logo,TemplatesId templateId, Dictionary<string, string> ObservacionesAdicionales = null, bool isB64 = false)
+        /// <param name="urlApi">URL de la API.</param>
+        /// <param name="url">URL de Services.</param>
+        /// <param name="user">Correo del usuario.</param>
+        /// <param name="password">Contraseña del usuario.</param>
+        /// <param name="proxy">Dirección del proxy.</param>
+        /// <param name="proxyPort">Puerto del proxy.</param>
+        public BasePdf(string urlApi, string url, string user, string password, string proxy, int proxyPort)
+            : base(urlApi, url, user, password, proxy, proxyPort)
         {
-            PdfResponseHandler handler = new PdfResponseHandler();
-            try
-            {
-                if (isB64 != true)
-                {
-                    var xmlFormat = xml.Replace("\"", "\'");
-                    var request = this.RequestPdf(xmlFormat, logo, templateId.ToString(), ObservacionesAdicionales);
-                    return handler.GetResponseRequest(request);
-                }
-                else
-                {
-                    var xmlString = Encoding.UTF8.GetString(Convert.FromBase64String(xml));
-                    var xmlFormat = xmlString.Replace("\"", "\'");
-                    var request = this.RequestPdf(xmlFormat, logo, templateId.ToString(), ObservacionesAdicionales);
-                    return handler.GetResponseRequest(request);
-                }
-            }
-            catch(Exception ex)
-            {
-                return handler.HandleException(ex);
-            }
-
         }
+
         /// <summary>
-        /// Servicio para generar PDF con plantillas personalizadas.
+        /// Genera un PDF utilizando una plantilla genérica.
         /// </summary>
-        /// <param name="xml">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
-        /// <param name="logo">El logotipo en base 64 .</param>
-        /// <param name="templateId">XML con formato UTF-8 del comprobante del cual se requiere el PDF </param>
-        /// <param name="ObservacionesAdicionales">Informacion extra </param>
-        /// <param name="isB64">Booleano que muestra si es b64 o no</param>
-        /// <returns>PdfResponse</returns>
-        public virtual PdfResponse GenerarPdf(string xml, string logo, string templateId, Dictionary<string, string> ObservacionesAdicionales = null, bool isB64 = false)
+        /// <param name="xml">Cadena XML del comprobante.</param>
+        /// <param name="logo">Logotipo en base64.</param>
+        /// <param name="templateId">Identificador de la plantilla.</param>
+        /// <param name="observacionesAdicionales">Información extra.</param>
+        /// <param name="isB64">Indica si el XML está en formato base64.</param>
+        /// <returns>Respuesta con el PDF generado.</returns>
+        public virtual PdfResponse GenerarPdf(string xml, string logo, TemplatesId templateId, Dictionary<string, string> observacionesAdicionales = null, bool isB64 = false)
         {
             PdfResponseHandler handler = new PdfResponseHandler();
             try
             {
-                if (isB64 != true)
-                {
-                    var xmlFormat = xml.Replace("\"", "\'");
-                    var request = this.RequestPdf(xmlFormat, logo, templateId, ObservacionesAdicionales);
-                    return handler.GetResponseRequest(request);
-                }
-                else {
-                    var xmlString = Encoding.UTF8.GetString(Convert.FromBase64String(xml));
-                    var xmlFormat = xmlString.Replace("\"", "\'");
-                    var request = this.RequestPdf(xmlFormat, logo, templateId, ObservacionesAdicionales);
-                    return handler.GetResponseRequest(request);
-                }
-                
+                var xmlContent = isB64 ? Encoding.UTF8.GetString(Convert.FromBase64String(xml)) : xml;
+                xmlContent = xmlContent.Replace("\"", "'");
+
+                var request = this.RequestPdf(xmlContent, logo, templateId.ToString(), observacionesAdicionales);
+                return handler.GetResponseRequest(request);
             }
             catch (Exception ex)
             {
                 return handler.HandleException(ex);
             }
-
         }
+
         /// <summary>
-        /// Servicio para regenerar PDF de un comprobante previamente timbrado.
+        /// Genera un PDF utilizando una plantilla personalizada.
+        /// </summary>
+        /// <param name="xml">Cadena XML del comprobante.</param>
+        /// <param name="logo">Logotipo en base64.</param>
+        /// <param name="templateId">Identificador de la plantilla.</param>
+        /// <param name="observacionesAdicionales">Información extra.</param>
+        /// <param name="isB64">Indica si el XML está en formato base64.</param>
+        /// <returns>Respuesta con el PDF generado.</returns>
+        public virtual PdfResponse GenerarPdf(string xml, string logo, string templateId, Dictionary<string, string> observacionesAdicionales = null, bool isB64 = false)
+        {
+            PdfResponseHandler handler = new PdfResponseHandler();
+            try
+            {
+                var xmlContent = isB64 ? Encoding.UTF8.GetString(Convert.FromBase64String(xml)) : xml;
+                xmlContent = xmlContent.Replace("\"", "'");
+
+                var request = this.RequestPdf(xmlContent, logo, templateId, observacionesAdicionales);
+                return handler.GetResponseRequest(request);
+            }
+            catch (Exception ex)
+            {
+                return handler.HandleException(ex);
+            }
+        }
+
+        /// <summary>
+        /// Regenera un PDF de un comprobante previamente timbrado.
         /// </summary>
         /// <param name="uuid">Folio fiscal del comprobante.</param>
-        /// <returns>PdfResponse</returns>
-        public virtual PdfResponse RegenerarPdf(Guid uuid)
+        /// <param name="logo">Logotipo en base64 (opcional).</param>
+        /// <param name="templateId">Identificador de la plantilla (opcional).</param>
+        /// <param name="extras">Información adicional (opcional).</param>
+        /// <returns>Respuesta con el PDF regenerado.</returns>
+        public virtual PdfResponse RegenerarPdf(Guid uuid, string logo = null, string templateId = null, Dictionary<string, string> extras = null)
         {
             PdfResponseHandler handler = new PdfResponseHandler();
             try
             {
                 new Validation(Url, User, Password, Token).ValidateHeaderParameters();
-                var request = this.RequestRegeneratePdf(uuid);
+
+                var request = this.RequestRegeneratePdf(uuid, logo, templateId, extras);
                 var response = handler.GetResponseRequest(request);
-                if (response.message == "Solicitud se proceso correctamente.") { response.status = "success"; }
+
+                if (response.message == "Solicitud se proceso correctamente.")
+                {
+                    response.status = "success";
+                }
+
                 return response;
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                return handler.HandleException(e);
+                return handler.HandleException(ex);
             }
         }
-
     }
 }
+
