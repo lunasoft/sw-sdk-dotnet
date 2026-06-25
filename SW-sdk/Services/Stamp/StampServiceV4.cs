9826b37d5d0e19b3facc1132fce07327be8cea88
Fernando Carrillo <108951984+FerCarrilloM@users.noreply.github.com>
2026-06-25 16:32:28 -0600

    add issueV4 class

--- SW-sdk/Services/Stamp/StampServiceV4.cs
+++ SW-sdk/Services/Stamp/StampServiceV4.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using SW.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
-using SW.Helpers;
 
 namespace SW.Services.Stamp
 {
